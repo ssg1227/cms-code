3c1d27e36bdf7a3a5fdd0d7dd5db9ac0d6275615
ssg1227 <steadyshan@gmail.com>
2024-06-26 08:57:18 -0700

    rainbow checker cake

--- assigments/ritchie-bakes/src/assets/gallery-files/lists-and-other/image-lists/speciality-cakes-non-chocolate.list.ts
+++ assigments/ritchie-bakes/src/assets/gallery-files/lists-and-other/image-lists/speciality-cakes-non-chocolate.list.ts
@@ -11,29 +11,7 @@ export class SpecialityCakesNonChocolate {
                 themeSummary:`</strong>Scenes, Objects, Structures, (some may be items containing mix modes of transport or something like an air dogfight)<br/>
                 Mumbai gets her due place and a dedicated theme; all Mumbai related sketches will be found in 'Mumbai Meri Jaan'`,
                 files: [ 
-                        {
-                              
-                              dateUploaded:  '06-06-2024',
-                              summaryLabel:  `Rainbow Checker Cake 1/2 kg `,
-                              fullFileName: `${this.imageRoot}rainbow-checker-cake-half-kg.jpeg`,
-                              description: `<ul><li> Rainbow Checker Cake Half Kg </li><li>More details coming soon </li></ul>`,
-
-                        },{
-                              
-                              dateUploaded:  '06-06-2024',
-                              summaryLabel:  `Rainbow Checker Cake 1 kg  `,
-                              fullFileName: `${this.imageRoot}rainbow-checker-cake-1-kg.jpeg`,
-                              description: `<ul><li> Rainbow Checker Cake 1 Kg </li><li>More details coming soon </li></ul>`,
-                              iterations: [
-                                    {fullFileName: `${this.imageRoot}rainbow-checker-cake-1-kg.jpeg`
-                                    , description: `<ul><li> Rainbow Checker Cake 1 Kg full cake</li><li>More details coming soon </li></ul>
-                                    ` },
-                                    {fullFileName: `${this.imageRoot}rainbow-checker-cake-1-kg-slice.jpeg`
-                                    , description: `<ul><li> Rainbow Checker Cake 1 Kg slice</li><li>More details coming soon </li></ul>
-                                    ` },
-                                    ]
-
-                        },{
+                       {
                               
                               dateUploaded:  '06-06-2024',
                               summaryLabel:  `Blueberry crush cake with whipped cream frosting `,
